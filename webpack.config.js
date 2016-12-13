ca8d62eb494f03fe79ff51670b05743b9d56f4e7
Christof Kälin <christof.kaelin@gmail.com>
2016-12-13 15:47:54 +0100

    more prod plugins

--- webpack.config.js
+++ webpack.config.js
@@ -152,12 +152,19 @@ const prod = {
     ]
   },
   plugins: [
-    new ExtractTextPlugin('static/css/[name].css'),
+    new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
       compress: {
         warnings: false
       }
-    })
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new ExtractTextPlugin('static/css/[name].css'),
   ]
 };
 
